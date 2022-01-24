f0a24bba144f19c8ba66a0cde9a208c10befb811
Nickita Taranov <nickita.taranov@gmail.com>
2022-01-25 00:20:43 +0300

    preallocate only twice more

--- src/Common/HashTable/HashTable.h
+++ src/Common/HashTable/HashTable.h
@@ -688,7 +688,7 @@ public:
     {
         if (Cell::need_zero_value_storage)
             this->zeroValue()->setZero();
-        grower.set(reserve_for_num_elements);
+        grower.setBufSize(std::max(grower.bufSize(), reserve_for_num_elements));
         alloc(grower);
     }
 
