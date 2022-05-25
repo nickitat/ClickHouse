e9a85c9365ae9f4a406c5646b0b91184a170d310
Nikita Taranov <nikita.taranov@clickhouse.com>
2022-05-25 11:52:16 +0000

    add tests

---
+++ tests/queries/0_stateless/02306_group_by_order_by_limit_optimization.sql
@@ -0,0 +1,9 @@
+create table tbl_agg(a UInt64, b UInt64) engine MergeTree order by (a, b);
+
+insert into tbl_agg select number, number from numbers_mt(100);
+
+select a from tbl_agg group by a, b order by a limit 5;
+
+select a from tbl_agg group by a, b order by a limit 5 offset 5;
+
+drop table tbl_agg;
