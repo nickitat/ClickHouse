0ee0aac611a637efd32c7e7b581049a95a7ebb37
Nikita Taranov <nikita.taranov@clickhouse.com>
2022-10-21 22:06:17 +0200

    fix tests

--- tests/queries/0_stateless/00410_aggregation_combinators_with_arenas.sql
+++ tests/queries/0_stateless/00410_aggregation_combinators_with_arenas.sql
@@ -7,4 +7,4 @@ DROP TABLE IF EXISTS arena;
 
 SELECT length(arrayReduce('groupUniqArray', [[1, 2], [1],  emptyArrayUInt8(), [1], [1, 2]]));
 SELECT min(x), max(x) FROM (SELECT length(arrayReduce('groupUniqArray', [hex(number), hex(number+1), hex(number)])) AS x FROM system.numbers LIMIT 100000);
-SELECT sum(length(runningAccumulate(x))) FROM (SELECT groupUniqArrayState(toString(number % 10)) AS x, number FROM (SELECT * FROM system.numbers LIMIT 11) GROUP BY number ORDER BY number);
+SELECT sum(length(r)) FROM (SELECT runningAccumulate(x) AS r FROM (SELECT groupUniqArrayState(toString(number % 10)) AS x, number FROM (SELECT * FROM system.numbers LIMIT 11) GROUP BY number ORDER BY number) ORDER BY x);
